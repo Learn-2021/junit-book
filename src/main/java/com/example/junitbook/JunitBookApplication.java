a81b0a324376f32f4badd661ff89c76f57f8951b
cocodori <moradulee@gmail.com>
2021-07-13 21:34:45 +0900

    2장

---
+++ src/main/java/com/example/junitbook/JunitBookApplication.java
@@ -0,0 +1,13 @@
+package com.example.junitbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JunitBookApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JunitBookApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/junitbook/ch02/ProfileTest.java
@@ -0,0 +1,40 @@
+package com.example.junitbook.ch02;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ProfileTest {
+    private Profile profile;
+    private BooleanQuestion question;
+    private Criteria criteria;
+
+    @BeforeEach
+    void create() {
+        profile = new Profile("Bull Hockey, Inc.");
+        question = new BooleanQuestion(1, "Got bonuses?");
+        criteria = new Criteria();
+    }
+
+    @Test
+    void matchAnswersFalseWhenMustMatchCriteriaNotMet() {
+        profile.add(new Answer(question, Bool.FALSE));
+        criteria.add(new Criterion(new Answer(question, Bool.TRUE), Weight.MustMatch));
+
+        boolean matches = profile.matches(criteria);
+
+        assertFalse(matches);
+    }
+
+    @Test
+    void matchAnswersTrueForAnyDontCareCriteria() {
+        profile.add(new Answer(question, Bool.FALSE));
+        criteria.add(new Criterion(new Answer(question, Bool.TRUE), Weight.DontCare));
+
+        boolean matches = profile.matches(criteria);
+
+        assertTrue(matches);
+    }
+
+}
